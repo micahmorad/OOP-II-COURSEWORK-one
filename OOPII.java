abcff2a42f75dea864e934069f536f1f80eb19cb
micahmorad <46837865+micahmorad@users.noreply.github.com>
2019-02-17 13:06:26 +0300

    Add files via upload

---
+++ OOPII.java
@@ -0,0 +1,295 @@
+
+package oop.ii;
+
+/**
+ *
+o
+ */
+import java.util.Scanner;
+public class OOPII {
+
+
+   
+    public static void main(String[] args) {
+        int mark[]= new int[7];
+        int i = 0;
+        String grd;
+        float sum =0;
+        Scanner scan = new Scanner (System.in);
+     System.out.println("Input Students Name");
+       String a=scan.next();
+     System.out.println(a);
+     
+     System.out.println("Input Course Name 1");
+       String b=scan.next();
+     System.out.println(b);
+     System.out.println("Input Marks Course Name 1");
+     
+         mark[i]=scan.nextInt();
+         sum= sum+ mark[i];
+     
+     if(mark[i]>80 && mark[i]<=100)
+     {
+         grd="A";
+         System.out.println("A");
+     }
+     else {
+         if(mark[i]>=70 && mark[i]<=79)
+         {
+             grd="B";
+             System.out.println("B");
+         }
+     }
+     if(mark[i]>60 && mark[i]<=69)
+     {
+         grd="C";
+         System.out.println("C");
+     }
+     else{
+         if(mark[i]>50 && mark[i]<=59)
+         {
+         grd="D";
+         System.out.println("D");
+         }  
+     } 
+     if(mark[i]>40 && mark[i]<=49)
+     {
+         grd="F";
+         System.out.println("F");
+    }   
+     else{
+         if(mark[i]>0 && mark[i]<=39)
+         {
+            grd="Invalid";
+            System.out.println("Invalid");
+         }
+     }
+     
+     System.out.println("Input Course Name 2");
+       String c=scan.next();
+     System.out.println(c);
+     System.out.println("Input Marks Course Name 2");
+     
+         mark[i]=scan.nextInt();
+         sum= sum+ mark[i];
+     
+     if(mark[i]>80 && mark[i]<=100)
+     {
+         grd="A";
+         System.out.println("A");
+     }
+     else {
+         if(mark[i]>=70 && mark[i]<=79)
+         {
+             grd="B";
+             System.out.println("B");
+         }
+     }
+     if(mark[i]>60 && mark[i]<=69)
+     {
+         grd="C";
+         System.out.println("C");
+     }
+     else{
+         if(mark[i]>50 && mark[i]<=59)
+         {
+         grd="D";
+         System.out.println("D");
+         }  
+     } 
+     if(mark[i]>40 && mark[i]<=49)
+     {
+         grd="F";
+         System.out.println("F");
+    }   
+     else{
+         if(mark[i]>0 && mark[i]<=39)
+         {
+            grd="Invalid";
+            System.out.println("Invalid");
+         }
+     }
+     
+      System.out.println("Input Course Name 3");
+       String d=scan.next();
+     System.out.println(d);
+     System.out.println("Input Marks Course Name 3");
+     
+         mark[i]=scan.nextInt();
+         sum= sum+ mark[i];
+     
+     if(mark[i]>80 && mark[i]<=100)
+     {
+         grd="A";
+         System.out.println("A");
+     }
+     else {
+         if(mark[i]>=70 && mark[i]<=79)
+         {
+             grd="B";
+             System.out.println("B");
+         }
+     }
+     if(mark[i]>60 && mark[i]<=69)
+     {
+         grd="C";
+         System.out.println("C");
+     }
+     else{
+         if(mark[i]>50 && mark[i]<=59)
+         {
+         grd="D";
+         System.out.println("D");
+         }  
+     } 
+     if(mark[i]>40 && mark[i]<=49)
+     {
+         grd="F";
+         System.out.println("F");
+    }   
+     else{
+         if(mark[i]>0 && mark[i]<=39)
+         {
+            grd="Invalid";
+            System.out.println("Invalid");
+         }
+     }
+     
+      System.out.println("Input Course Name 4");
+       String e=scan.next();
+     System.out.println(e);
+     System.out.println("Input Marks Course Name 4");
+    
+         mark[i]=scan.nextInt();
+         sum= sum+ mark[i];
+     
+     if(mark[i]>80 && mark[i]<=100)
+     {
+         grd="A";
+         System.out.println("A");
+     }
+     else {
+         if(mark[i]>=70 && mark[i]<=79)
+         {
+             grd="B";
+             System.out.println("B");
+         }
+     }
+     if(mark[i]>60 && mark[i]<=69)
+     {
+         grd="C";
+         System.out.println("C");
+     }
+     else{
+         if(mark[i]>50 && mark[i]<=59)
+         {
+         grd="D";
+         System.out.println("D");
+         }  
+     } 
+     if(mark[i]>40 && mark[i]<=49)
+     {
+         grd="F";
+         System.out.println("F");
+    }   
+     else{
+         if(mark[i]>0 && mark[i]<=39)
+         {
+            grd="Invalid";
+            System.out.println("Invalid");
+         }
+     }
+     
+      System.out.println("Input Course Name 5");
+       String f=scan.next();
+     System.out.println(f);
+     System.out.println("Input Marks Course Name 5");
+    
+         mark[i]=scan.nextInt();
+         sum= sum+ mark[i];
+     
+     if(mark[i]>80 && mark[i]<=100)
+     {
+         grd="A";
+         System.out.println("A");
+     }
+     else {
+         if(mark[i]>=70 && mark[i]<=79)
+         {
+             grd="B";
+             System.out.println("B");
+         }
+     }
+     if(mark[i]>60 && mark[i]<=69)
+     {
+         grd="C";
+         System.out.println("C");
+     }
+     else{
+         if(mark[i]>50 && mark[i]<=59)
+         {
+         grd="D";
+         System.out.println("D");
+         }  
+     } 
+     if(mark[i]>40 && mark[i]<=49)
+     {
+         grd="F";
+         System.out.println("F");
+    }   
+     else{
+         if(mark[i]>0 && mark[i]<=39)
+         {
+            grd="Invalid";
+            System.out.println("Invalid");
+         }
+     }
+     
+      System.out.println("Input Course Name 6");
+       String g=scan.next();
+     System.out.println(g);
+     System.out.println("Input Marks Course Name 6");
+    
+         mark[i]=scan.nextInt();
+         sum =sum+ mark[i];
+     
+     if(mark[i]>80 && mark[i]<=100)
+     {
+         grd="A";
+         System.out.println("A");
+     }
+     else {
+         if(mark[i]>=70 && mark[i]<=79)
+         {
+             grd="B";
+             System.out.println("B");
+         }
+     }
+     if(mark[i]>60 && mark[i]<=69)
+     {
+         grd="C";
+         System.out.println("C");
+     }
+     else{
+         if(mark[i]>50 && mark[i]<=59)
+         {
+         grd="D";
+         System.out.println("D");
+         }  
+     } 
+     if(mark[i]>40 && mark[i]<=49)
+     {
+         grd="F";
+         System.out.println("F");
+    }   
+     else{
+         if(mark[i]>0 && mark[i]<=39)
+         {
+            grd="Invalid";
+            System.out.println("Invalid");
+         }
+     }
+     
+     
+    }
+}
